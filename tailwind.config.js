04be5b9fd0e894f92c73db012090c413f5756986
Maurício de Carvalho <mauriciocr223@gmail.com>
2023-07-09 21:46:49 -0300

    chore: imported fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      openSans: ['"Open Sans"'],
+      lexend: ["Lexend"],
+    },
   },
   plugins: [],
 };
